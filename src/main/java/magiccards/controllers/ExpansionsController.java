6da013e8b9f6a05580cdea8fdb7471b2a7773a6d
marcusvsantanao <marcusvsantanao@gmail.com>
2019-07-27 17:08:35 -0300

    WIP creation and search working propertly

---
+++ src/main/java/magiccards/controllers/ExpansionsController.java
@@ -0,0 +1,48 @@
+package magiccards.controllers;
+
+import magiccards.entities.Expansion;
+import magiccards.repositories.ExpansionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("expansions")
+public class ExpansionsController {
+
+
+	@Autowired
+	private ExpansionRepository expansionRepository;
+
+	@RequestMapping(value = "{id}", method = RequestMethod.GET, produces = "application/json")
+	public Expansion getExpansionById(@PathVariable("id") String expansionId) {
+		Integer id = Integer.parseInt(expansionId);
+		return expansionRepository.findOne(id);
+
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
+	public Page<Expansion> getExpansions(@RequestParam(value="page")int pageNumber, @RequestParam int size) {
+
+		Pageable page = new PageRequest(pageNumber, size);
+
+		return expansionRepository.findAll(page);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST, consumes = "application/json")
+	public void create(@RequestBody Expansion exp) {
+		expansionRepository.save(exp);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.PUT, consumes = "application/json")
+	public void update(@RequestBody Expansion exp) {
+		expansionRepository.save(exp);
+	}
+
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id")Integer id) {
+		expansionRepository.delete(id);
+	}
+}
---
+++ src/main/java/magiccards/entities/Expansion.java
@@ -0,0 +1,37 @@
+package magiccards.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "magicexpansion")
+public class Expansion {
+    @Id
+    @Column(name = "expansionid")
+    private Integer expansionId;
+    @Column(name = "name", columnDefinition = "text")
+    private String name;
+    @Column(name = "ptbrname", columnDefinition = "text")
+    private String ptBrName;
+    @Column(name = "linkname", columnDefinition = "text")
+    private String linkName;
+    @Column(name = "code", columnDefinition = "text")
+    private String code;
+    @Column(name = "launchdate")
+    private Date launchDate;
+    @Column(name = "expansioncategoryid",columnDefinition = "smallint")
+    private Integer expansionCategoryId;
+    @Column(name = "ispromo")
+    private boolean isPromo;
+    @Column(name = "islegal")
+    private boolean isLegal;
+
+}
